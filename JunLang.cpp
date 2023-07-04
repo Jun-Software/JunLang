196ff7f76a9f22762d1bab3191fa1016cfd25057
Jiang Chengjun <107529938+lemonorangeapple@users.noreply.github.com>
2023-07-04 10:21:04 +0800

    Add files via upload

--- JunLang.cpp
+++ JunLang.cpp
@@ -10,7 +10,13 @@ using namespace std;
 #define _DEFAULT_BUFFER_SIZE_ 1024
 string identifiers[] = {
     "output",
-    "wrap"
+    "wrap",
+    "new",
+    "set"
+};
+struct Variable {
+    int value;
+    string name;
 };
 
 
@@ -22,6 +28,16 @@ struct UnknowIdentifier : public exception {
 struct FileCannotOpen : public exception {
     const char * what () const throw () {
         return "File cannot open.";
+    }
+};
+struct DuplicateVariableName : public exception {
+    const char * what () const throw () {
+        return "Duplicate variable name.";
+    }
+};
+struct UndeclaredVariable : public exception {
+    const char * what () const throw () {
+        return "Undeclared variable.";
     }
 };
 vector<string> split(string str, char delim) {
@@ -46,6 +62,8 @@ int main (int argc, char* argv[]) {
             bufferSize = _DEFAULT_BUFFER_SIZE_;
         }
         char buffer[bufferSize];
+        Variable variables[bufferSize];
+        int variableCount = 0;
         ifstream file(fileName);
         if (!file.is_open()) {
             throw FileCannotOpen();
@@ -58,17 +76,58 @@ int main (int argc, char* argv[]) {
                 for (int i = 0; i < (sizeof(identifiers) / sizeof(string)); i++) {
                     if (identifiers[i] == *it) {
                         unknow = false;
+                        break;
                     }
                 }
                 if (!unknow) {
                     if (*it == identifiers[0]) {
-                        cout << *(it + 1);
+                        if ((*(it + 1))[0] != '"') {
+                            bool variable = false;
+                            for (int i = 0; i <= variableCount; i++) {
+                                if (variables[i].name == *(it + 1)) {
+                                    cout << variables[i].value;
+                                    variable = true;
+                                    break;
+                                }
+                            }
+                            if (!variable) {
+                                throw UndeclaredVariable();
+                            }
+                        }
+                        cout << string(*(it + 1)).substr(1, strlen((*(it + 1)).c_str()) - 2);
                         break;
                     }
                     if (*it == identifiers[1]) {
                         cout << endl;
                     }
-                    // TODO =====================================================================================================
+                    if (*it == identifiers[2]) {
+                        bool duplicate = false;
+                        for (int i = 0; i <= variableCount; i++) {
+                            if (variables[i].name == *(it + 1)) {
+                                duplicate = true;
+                                break;
+                            }
+                        }
+                        if (duplicate) {
+                            throw DuplicateVariableName();
+                        }
+                        variables[variableCount++].name = *(it + 1);
+                        break;
+                    }
+                    if (*it == identifiers[3]) {
+                        bool undeclared = true;
+                        for (int i = 0; i <= variableCount; i++) {
+                            if (variables[i].name == *(it + 1)) {
+                                variables[i].value = atoi((*(it + 2)).c_str());
+                                undeclared = false;
+                                break;
+                            }
+                        }
+                        if (undeclared) {
+                            throw UndeclaredVariable();
+                        }
+                        break;
+                    }
                 }
                 else {
                     throw UnknowIdentifier();
