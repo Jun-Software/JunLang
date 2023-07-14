b1f5a0af29347c8977f5aab66f0c8fc17ea5f662
lemonorangeapple <107529938+lemonorangeapple@users.noreply.github.com>
2023-07-14 11:21:20 +0800

    v1.12

--- JunLang.cpp
+++ JunLang.cpp
@@ -134,20 +134,6 @@ int main(int argc, char* argv[]) {
                         bool undeclared = true;
                         for (int i = 0; i <= variableCount; i++) {
                             if (variables[i].name == *(it + 1)) {
-                                variables[i].value = atoi((*(it + 2)).c_str());
-                                undeclared = false;
-                                break;
-                            }
-                        }
-                        if (undeclared) {
-                            throw UndeclaredVariable();
-                        }
-                        break;
-                    }
-                    if (*it == identifiers[4]) {
-                        bool undeclared = true;
-                        for (int i = 0; i <= variableCount; i++) {
-                            if (variables[i].name == *(it + 1)) {
                                 if (isInteger(*(it + 2))) {
                                     variables[i].value = atoi((*(it + 2)).c_str());
                                     undeclared = false;
@@ -166,6 +152,22 @@ int main(int argc, char* argv[]) {
                             }
                         }
                         if (undeclared) {
+                            throw UndeclaredVariable();
+                        }
+                        break;
+                    }
+                    if (*it == identifiers[4]) {
+                    	bool undeclared = true;
+                        for (int i = 0; i <= variableCount; i++) {
+                            if (variables[i].name == *(it + 1)) {
+                            	long double temp;
+                            	cin >> temp;
+                            	variables[i].value = temp;
+                            	undeclared = false;
+                            	break;
+                            }
+						}
+						if (undeclared) {
                             throw UndeclaredVariable();
                         }
                         break;
