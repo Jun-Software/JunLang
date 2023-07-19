08bcfb5902e9f2037cfe1fe9f5784c0d1b2beae1
lemonorangeapple <jcj1947725596@hotmail.com>
2023-07-19 22:21:38 +0800

    v1.13.1

--- JunLang.cpp
+++ JunLang.cpp
@@ -160,7 +160,7 @@ int main(int argc, char* argv[]) {
                             }
 						}
 						if (undeclared) {
-                            throw UndeclaredVariable();
+                            cerr << "[ERROR] Undeclared variable.\n";
                         }
                         break;
                     }
---
+++ make.sh
@@ -0,0 +1,2 @@
+g++ JunLang.cpp -oJunLang.out -std=c++11 -static
+/usr/bin/i686-w64-mingw32-g++ JunLang.cpp -oJunLang.exe -std=c++11 -static
