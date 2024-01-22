036385356d44ca7c50176fbf982a3ae5d689fb62
lemonorangeapple <jcj1947725596@hotmail.com>
2024-01-22 14:12:55 +0800

    update

--- src/update.hpp
+++ src/update.hpp
@@ -8,30 +8,34 @@
 
 // execute external command and store result in result
 string execute(const char *command) {
-  char buffer[1024];
-  char result[1024] = {0};
-  FILE *ptr;
-  strcpy(result, command);
-  if((ptr = popen(result, "r"))) {
-      while (fgets(buffer, 1024, ptr)) {
-         strcat(result, buffer);
-         if (strlen(result) > 1024) {
-             break;
-         }
-      }
-      pclose(ptr);
-      ptr = NULL;
-  }
-  return result;
+    char buffer[1024];
+    char result[1024] = {0};
+    FILE *ptr;
+    strcpy(result, command);
+    if((ptr = popen(result, "r"))) {
+        while (fgets(buffer, 1024, ptr)) {
+            strcat(result, buffer);
+            if (strlen(result) > 1024) {
+                break;
+            }
+        }
+        pclose(ptr);
+        ptr = NULL;
+    }
+    return result;
 }
 
 // update function
 void update() {
-  // get version number from https://junlang.imjcj.eu.org/VERSION
-  string result = execute("curl -s https://junlang.imjcj.eu.org/VERSION").substr(44);
-  // compare version number with _VERSION_
-  if (result != _VERSION_ + '\n') {
-      cout << "Warning: Please update: \"https://junlang.imjcj.eu.org\"" << endl;
-  }
-  return;
+    // get version number from https://junlang.imjcj.eu.org/VERSION
+    string result = execute("curl -s https://junlang.imjcj.eu.org/VERSION");
+    #ifdef _WIN32
+        result = result.substr(44);
+    #endif
+    // compare version number with _VERSION_
+    if (result != _VERSION_ + '\n') {
+        cout << result << endl;
+        cout << "Warning: Please update: \"https://junlang.imjcj.eu.org\"" << endl;
+    }
+    return;
 }
