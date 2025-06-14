15c07c6f8b4f335d57698e623055b135cefdbfd7
lemonorangeapple <jcj1947725596@hotmail.com>
2025-06-14 16:37:10 +0800

    新时代

--- src/init.hpp
+++ src/init.hpp
@@ -14,7 +14,7 @@
 using namespace std;
 #pragma GCC std("c++11")
 // Version of the program
-const string _VERSION_ = "v1.14.13";
+const string _VERSION_ = "v1.15.00";
 // Buffer size
 const int _BUFFER_SIZE_ = 1024;
 // All identifiers.
